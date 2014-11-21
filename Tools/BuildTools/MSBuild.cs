8cbae1d87999dc674376c278fd11579f2a74d330
tostercx <tostercx@gmail.com>
2014-11-21 02:15:30 +0200

    stdout test 2

--- Tools/BuildTools/MSBuild.cs
+++ Tools/BuildTools/MSBuild.cs
@@ -36,20 +36,17 @@ namespace BuildTools
             psi.Arguments = '"' + solutionPath + "\" " + args;
             psi.WorkingDirectory = Path.GetDirectoryName(solutionPath);
 
-            // msbuild seems to be failing withouth shellexec for some reason
-            psi.UseShellExecute = true;
+            psi.UseShellExecute = false;
             psi.RedirectStandardOutput = false;
             psi.RedirectStandardError = false;
 
             Console.WriteLine("Executing " + psi.FileName + " " + psi.Arguments);
             var p = Process.Start(psi);
             p.WaitForExit();
-            ConsoleColor original = Console.ForegroundColor;
-            //Console.WriteLine(p.StandardOutput.ReadToEnd());
-            Console.ForegroundColor = ConsoleColor.Red;
-            //Console.Write(p.StandardError.ReadToEnd());
+            
             if (p.ExitCode != 0) Console.WriteLine("Visual Studio may have encountered errors during the build.");
-            Console.ForegroundColor = original;
+
+            Console.WriteLine("Exit code " + p.ExitCode.ToString());
             return p.ExitCode;
         }
 
